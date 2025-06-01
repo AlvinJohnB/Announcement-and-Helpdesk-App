51b57b2fa2063f57b0bb7acfe87b19f53455dc03
alvinbregana <alvinbregana@gmail.com>
2025-06-01 15:05:50 +0800

    Initial commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+module.exports = async (req, res, next) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Fetch full user from DB
+    const user = await User.findById(decoded.user.id);
+    if (!user) {
+      return res.status(401).json({ msg: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
